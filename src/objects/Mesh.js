832c79a2e3a7422e24037c4b9dda730aded2edd5
Wittmann Tobias <twittmann@cadenas.internal>
2018-05-28 15:12:40 +0200

    fix: if there is no geometrie doe not intersect
    (cherry picked from commit f856b93e7c8c86daaa980c3eec71134f29e825fe)

--- src/objects/Mesh.js
+++ src/objects/Mesh.js
@@ -239,7 +239,9 @@ Mesh.prototype = Object.assign( Object.create( Object3D.prototype ), {
 			if ( material === undefined ) return;
 
 			// Checking boundingSphere distance to ray
-
+			if ( !geometry ) {
+				return;
+			}
 			if ( geometry.boundingSphere === null ) geometry.computeBoundingSphere();
 
 			sphere.copy( geometry.boundingSphere );
