0d12a7cbebe8136719a36a5854025ac78284f732
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-07-29 17:19:29 -0700

    Separate the current material function
    (cherry picked from commit b454a81b41779e49811ba81e3a8d12523ae18fc3)

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -732,10 +732,18 @@ function WebGLRenderer( parameters ) {
 
 	};
 
+	var currentMaterial = null;
 	var uploadMesh = new Mesh();
 	var emptyState = new WebGLRenderState();
 	emptyState.init();
-	this.drawGeometry = function ( camera, fog, geometry, material, object, group ) {
+	this.setMaterial = function ( material ) {
+
+		_currentMaterialId = - 1;
+		currentMaterial = material;
+
+	};
+
+	this.drawGeometry = function ( camera, fog, geometry, object, group ) {
 
 		if ( vr.enabled ) {
 
@@ -748,8 +756,6 @@ function WebGLRenderer( parameters ) {
 			currentRenderState = emptyState;
 
 		}
-
-		_currentMaterialId = - 1;
 
 		if ( camera !== _currentCamera ) {
 
@@ -768,7 +774,7 @@ function WebGLRenderer( parameters ) {
 
 		object.modelViewMatrix.multiplyMatrices( camera.matrixWorldInverse, object.matrixWorld );
 		object.normalMatrix.getNormalMatrix( object.modelViewMatrix );
-		this.renderBufferDirect( camera, fog, geometry, material, object, group );
+		this.renderBufferDirect( camera, fog, geometry, currentMaterial, object, group );
 		state.setPolygonOffset( false );
 
 		if ( currentRenderState === emptyState ) {
